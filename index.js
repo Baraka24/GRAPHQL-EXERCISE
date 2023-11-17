f3f38c3c2dc37dd503b3ee7da6b561a0d3058254
Baraka24 <bkinywa24@gmail.com>
2023-11-17 23:21:12 +0200

    GRAPHQL-EXERCISE

---
+++ index.js
@@ -0,0 +1,12 @@
+const {gql} = require("apollo-server");
+
+const typeDefs = gql`
+    type Student {
+        id: ID!
+        firstName: String!
+        lastName: String!
+    }
+    type Query {
+    Students: [Student!]!
+    }
+`;
